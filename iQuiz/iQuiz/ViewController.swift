71fc1d8dfad257cbcc09d6a441ed3dfd446f5c6d
“vivianngu3” <vn23@uw.edu>
2021-05-05 22:18:01 -0700

    added assets & alert

---
+++ iQuiz/iQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Vivian Nguyen on 5/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
